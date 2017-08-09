bc4865ce1cbbed74432b10eebace96b18ca75a6b
Tyler Fischer <fischt77@uwosh.edu>
2017-08-09 09:15:49 -0500

    functions as varibles
    Example of how javascript allows user's to pass functions as varibles.

---
+++ functionsAsVaribles.js
@@ -0,0 +1,9 @@
+let repeat = (operation , num) => {
+    for(let i = 0; i < num; i++ ) {
+        operation(num)
+    }
+}
+
+let exampleFunction = () => true;
+repeat(exampleFunction, 5);
+module.exports = repeat;
